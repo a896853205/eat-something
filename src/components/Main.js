092b82c949413bb064d9e83fe9c78b3b940bb3bb
a896853205 <896853205@qq.com>
2017-01-11 15:50:39 +0800

    style of base

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,18 +3,33 @@ require('styles/App.css');
 
 import React from 'react';
 
+class RandomMain extends React.Component {
+  render() {
+    return (
+      <div className="info-main-div">
+        <div className="info-show-div">{this.props.showInfo}</div>
+        <button className="info-control-button">看看吃啥</button>
+      </div>
+    );
+  }
+}
 
 class AppComponent extends React.Component {
+  state = {showFood:"中午吃什么?"};
   render() {
     return (
       <div className="index">
-        <div></div>
+        <RandomMain showInfo={this.state.showFood}/>
       </div>
     );
   }
 }
 
 AppComponent.defaultProps = {
+  foodArray:["腊汁肉拌饭","淮南牛肉汤饭","老林子鸡鸡腿"],
 };
 
+/*AppComponent.getInitialState = {
+  showFood:"中午吃什么呢?"
+};*/
 export default AppComponent;
