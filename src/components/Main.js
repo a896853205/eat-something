24f86bd701ac6ed957c261b7428a56b5cc56f3ff
Qccccc's laptop <896853205@qq.com>
2017-02-03 17:40:09 +0800

    add RandomMain component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,34 +2,46 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+/*食物随机数组*/
+const FOODARRAY = ["腊汁肉拌饭","淮南牛肉汤饭","老林子鸡鸡腿"];
+/* 随机函数*/
+let randomArray = array => {
+  let index = Math.floor(Math.random()*array.length);
+  return array[index];
+};
 
+/* 中间控件 */
 class RandomMain extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showInfo:"中午吃什么呢?"
+    };
+  };
+  changeShow(){
+    setInterval(() => {
+      this.setState({showInfo:randomArray(FOODARRAY)});
+    },100);
+  };
   render() {
     return (
       <div className="info-main-div">
-        <div className="info-show-div">{this.props.showInfo}</div>
-        <button className="info-control-button">看看吃啥</button>
+        <div className="info-show-div">{this.state.showInfo}</div>
+        <button className="info-control-button" onClick={this.changeShow.bind(this)}>看看吃啥</button>
       </div>
     );
   }
 }
 
+/* 主舞台控件 */
 class AppComponent extends React.Component {
-  state = {showFood:"中午吃什么?"};
   render() {
     return (
       <div className="index">
-        <RandomMain showInfo={this.state.showFood}/>
+        <RandomMain/>
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-  foodArray:["腊汁肉拌饭","淮南牛肉汤饭","老林子鸡鸡腿"],
-};
-
-/*AppComponent.getInitialState = {
-  showFood:"中午吃什么呢?"
-};*/
 export default AppComponent;
